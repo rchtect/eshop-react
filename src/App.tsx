2cc35322ef6bbd9dbe4a9ad0986022bcbea9770e
Kawium <kawanse0@gmail.com>
2022-03-15 15:04:43 +0100

    added vite, react-ts and tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
