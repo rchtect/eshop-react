610738b8554585b34370bf361f91d28051680d88
Kawium <kawanse0@gmail.com>
2022-03-16 15:05:15 +0100

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Navbar() {
-  return <div className="underline">Navbar</div>;
+  return (
+    <div className="flex bg-[#090B20] p-4">
+      <div className="text-[#90FE9B]">Shoppy</div>
+    </div>
+  );
 }
 
 export default Navbar;
