9cba6ec00331d9944e4fdbc816d9ae12abf694e9
Kawium <kawanse0@gmail.com>
2022-03-16 15:46:21 +0100

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,12 @@ import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 function Navbar() {
   return (
     <div className="flex justify-between p-5 items-center select-none">
-      <div className="text-green cursor-pointer text-4xl font-bold">Shoppy</div>
+      <div className="text-green cursor-pointer font-bold">
+        <div className="flex flex-col	">
+          <h1 className="text-4xl">Shoppy</h1>
+          <p className="text-white">Pay less. Buy Later.</p>
+        </div>
+      </div>
       <div className="cursor-pointer hover:bg-slate-500 rounded-md p-3 scale-125">
         <ShoppingCartIcon />
       </div>
