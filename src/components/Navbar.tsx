e561a8cd47af4ad3789fd9413ac1184de303192c
Kawium <kawanse0@gmail.com>
2022-03-16 15:34:42 +0100

    finished navbar and added custom colors to tailwind config

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 function Navbar() {
   return (
-    <div className="flex bg-[#090B20] p-4">
-      <div className="text-[#90FE9B]">Shoppy</div>
+    <div className="flex justify-between p-5 items-center select-none">
+      <div className="text-green cursor-pointer text-4xl font-bold">Shoppy</div>
+      <div className="cursor-pointer hover:bg-slate-500 rounded-md p-3 scale-125">
+        <ShoppingCartIcon />
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: "#90FE9B",
+      },
+    },
   },
   plugins: [],
 };
