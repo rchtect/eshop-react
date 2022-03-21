d3543ec67bb5e1b4247e174793f13680e7d14045
rchtect <90619276+rchtect@users.noreply.github.com>
2022-03-21 20:45:02 +0100

    added dynamics to carousel

---
+++ src/maincontent/carousel-data.tsx
@@ -0,0 +1,22 @@
+
+const carouselData = [
+    {
+        id: 1,
+        title: "Payments",
+        subtitle: "We make payments easy for you by providing multiple options.",
+        icon: "shopping_basket"
+    },
+    {
+        id: 2,
+        title: "Delivery",
+        subtitle: "Fast deliveries free of charge.",
+        icon: "local_shipping"
+    },
+    {
+        id: 3,
+        title: "Membership",
+        subtitle: "Become a member to enjoy benefits.",
+        icon: "loyalty"
+    }
+]
+export default carouselData
