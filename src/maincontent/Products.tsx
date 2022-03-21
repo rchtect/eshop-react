9e1f6855f7b2a70e50a20508a0da5ddef635b6bc
Kawium <kawanse0@gmail.com>
2022-03-21 20:10:44 +0100

    Added products.tsx and Images folder

---
+++ src/maincontent/Products.tsx
@@ -0,0 +1,210 @@
+import React from "react";
+
+function Products() {
+  return (
+    <div className="flex p-10 gap-6 flex-wrap justify-center ">
+      <div className="flex flex-col p-10 max-w-sm  bg-slate-600 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 transition-all hover:scale-105">
+        <img
+          className="select-none"
+          src="/src/Images/product-1.png"
+          alt="first-product"
+        />
+        <div className="flex flex-col mt-10">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga facilis
+          voluptate officia quidem corporis distinctio, vel optio quis explicabo
+          autem nobis itaque ea dicta. Ad ea nostrum quia architecto officiis.
+        </div>
+        <div>
+          <div className="flex justify-between items-center mt-10">
+            <span className="text-3xl font-bold text-gray-900 dark:text-white">
+              $600
+            </span>
+            <a
+              href="#"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Add to cart
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col p-10 max-w-sm bg-slate-600 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 transition-all hover:scale-105">
+        <img
+          className="select-none"
+          src="/src/Images/product-1.png"
+          alt="first-product"
+        />
+        <div className="flex flex-col mt-10">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga facilis
+          voluptate officia quidem corporis distinctio, vel optio quis explicabo
+          autem nobis itaque ea dicta. Ad ea nostrum quia architecto officiis.
+        </div>
+        <div>
+          <div className="flex justify-between items-center mt-10">
+            <span className="text-3xl font-bold text-gray-900 dark:text-white">
+              $600
+            </span>
+            <a
+              href="#"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Add to cart
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col p-10 max-w-sm bg-slate-600 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 transition-all hover:scale-105">
+        <img
+          className="select-none"
+          src="/src/Images/product-1.png"
+          alt="first-product"
+        />
+        <div className="flex flex-col mt-10">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga facilis
+          voluptate officia quidem corporis distinctio, vel optio quis explicabo
+          autem nobis itaque ea dicta. Ad ea nostrum quia architecto officiis.
+        </div>
+        <div>
+          <div className="flex justify-between items-center mt-10">
+            <span className="text-3xl font-bold text-gray-900 dark:text-white">
+              $600
+            </span>
+            <a
+              href="#"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Add to cart
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col p-10 max-w-sm bg-slate-600 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 transition-all hover:scale-105">
+        <img
+          className="select-none"
+          src="/src/Images/product-1.png"
+          alt="first-product"
+        />
+        <div className="flex flex-col mt-10">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga facilis
+          voluptate officia quidem corporis distinctio, vel optio quis explicabo
+          autem nobis itaque ea dicta. Ad ea nostrum quia architecto officiis.
+        </div>
+        <div>
+          <div className="flex justify-between items-center mt-10">
+            <span className="text-3xl font-bold text-gray-900 dark:text-white">
+              $600
+            </span>
+            <a
+              href="#"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Add to cart
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col p-10 max-w-sm bg-slate-600 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 transition-all hover:scale-105">
+        <img
+          className="select-none"
+          src="/src/Images/product-1.png"
+          alt="first-product"
+        />
+        <div className="flex flex-col mt-10">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga facilis
+          voluptate officia quidem corporis distinctio, vel optio quis explicabo
+          autem nobis itaque ea dicta. Ad ea nostrum quia architecto officiis.
+        </div>
+        <div>
+          <div className="flex justify-between items-center mt-10">
+            <span className="text-3xl font-bold text-gray-900 dark:text-white">
+              $600
+            </span>
+            <a
+              href="#"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Add to cart
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col p-10 max-w-sm bg-slate-600 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 transition-all hover:scale-105">
+        <img
+          className="select-none"
+          src="/src/Images/product-1.png"
+          alt="first-product"
+        />
+        <div className="flex flex-col mt-10">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga facilis
+          voluptate officia quidem corporis distinctio, vel optio quis explicabo
+          autem nobis itaque ea dicta. Ad ea nostrum quia architecto officiis.
+        </div>
+        <div>
+          <div className="flex justify-between items-center mt-10">
+            <span className="text-3xl font-bold text-gray-900 dark:text-white">
+              $600
+            </span>
+            <a
+              href="#"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Add to cart
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col p-10 max-w-sm bg-slate-600 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 transition-all hover:scale-105">
+        <img
+          className="select-none"
+          src="/src/Images/product-1.png"
+          alt="first-product"
+        />
+        <div className="flex flex-col mt-10">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga facilis
+          voluptate officia quidem corporis distinctio, vel optio quis explicabo
+          autem nobis itaque ea dicta. Ad ea nostrum quia architecto officiis.
+        </div>
+        <div>
+          <div className="flex justify-between items-center mt-10">
+            <span className="text-3xl font-bold text-gray-900 dark:text-white">
+              $600
+            </span>
+            <a
+              href="#"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Add to cart
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col p-10 max-w-sm bg-slate-600 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 transition-all hover:scale-105">
+        <img
+          className="select-none"
+          src="/src/Images/product-1.png"
+          alt="first-product"
+        />
+        <div className="flex flex-col mt-10">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga facilis
+          voluptate officia quidem corporis distinctio, vel optio quis explicabo
+          autem nobis itaque ea dicta. Ad ea nostrum quia architecto officiis.
+        </div>
+        <div>
+          <div className="flex justify-between items-center mt-10">
+            <span className="text-3xl font-bold text-gray-900 dark:text-white">
+              $600
+            </span>
+            <a
+              href="#"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Add to cart
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Products;
--- src/maincontent/maincontent.tsx
+++ src/maincontent/maincontent.tsx
@@ -1,13 +1,13 @@
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import Carousel from './carousel';
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import Carousel from "./carousel";
+import Products from "./Products";
 function MainContent() {
-  
-    return (
-            <div className="MainContent flex justify-center items-center">
-                <Carousel/>
-            </div>
-    );
-  }
-  
-  export default MainContent;
-  
+  return (
+    <div className="MainContent flex flex-col justify-center items-center">
+      <Carousel />
+      <Products />
+    </div>
+  );
+}
+
+export default MainContent;
